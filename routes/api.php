e69d4e1ce2f4bc71a6a5c24f29e40c1f320fa730
jeantinocotech <jeantinocotech@gmail.com>
2025-06-23 15:00:48 +0200

    API webhook 123

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotmartWebhookController;
 
-Route::prefix('api')->group(function () {
-    Route::post('/cart/hotmart/webhook', [HotmartWebhookController::class, 'handle']);
-});
+Route::post('/cart/hotmart/webhook', [HotmartWebhookController::class, 'handle']);
 
