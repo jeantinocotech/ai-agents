612560451811839d9954fbc5799d9eeac77aa53a
jeantinocotech <jeantinocotech@gmail.com>
2025-06-23 14:13:55 +0200

    API webhook

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AgentController;
 use App\Http\Controllers\HotmartWebhookController;
 
-
-Route::middleware('auth:sanctum')->group(function () {
-    // Protected API routes go here
-});
-
 Route::post('/cart/hotmart/webhook', [HotmartWebhookController::class, 'handle']);
-
-
